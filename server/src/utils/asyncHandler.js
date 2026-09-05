452a54f41c868684bdf9bbdc7c1f5c913e3e56db
UTKARSH VERMA <utkarshvermaone@gmail.com>
2026-09-05 19:37:13 +0530

    Add files via upload

---
+++ server/src/utils/asyncHandler.js
@@ -0,0 +1,6 @@
+/** Wrap an async route handler so rejected promises reach the error middleware. */
+function asyncHandler(fn) {
+  return (req, res, next) => Promise.resolve(fn(req, res, next)).catch(next)
+}
+
+module.exports = asyncHandler
